ef2b96ce47ac7e610390cd8bfe7b3ec71c7e38d1
vladilen11 <vladilenaksana@163.com>
2024-03-30 20:07:36 +0800

    add obelisk sdk

---
+++ frontend/src/chain/config.ts
@@ -0,0 +1,8 @@
+import { Network } from '@0xobelisk/aptos-client';
+
+const NETWORK: Network = Network.DEVNET;
+
+const PACKAGE_ID =
+	'0xc3a0b686e18cd2f49ee4a8ed721d11ff9483f00bf66258ff38355163df5844ee';
+
+export { NETWORK, PACKAGE_ID };
